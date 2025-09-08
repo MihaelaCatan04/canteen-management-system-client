cee0d735d1d411ee37205cda713116a1272da331
Mihaela Catan <mihaelacatan7@gmail.com>
2025-09-08 23:46:44 +0300

    Add conditional loading message

--- canteen-management-system-client/src/pages/LandingPage.jsx
+++ canteen-management-system-client/src/pages/LandingPage.jsx
@@ -1,10 +1,31 @@
 import LoginLayout from "../layouts/LandingPage";
 import LandingPageLeft from "../components/LandingPage/LandingPageLeft";
 import LandingPageRight from "../components/LandingPage/LandingPageRight";
+import { useState, useEffect } from "react";
+
 
 const LandingPage = () => {
+  const [right, setRight] = useState(null);
+  const [left, setLeft] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setRight(<LandingPageRight />);
+  //     setLeft(<LandingPageLeft />);
+  //     setIsPending(false);
+  //   }, 1000);
+  // }, []);
+
+  useEffect(() => {
+    setRight(<LandingPageRight />);
+    setLeft(<LandingPageLeft />);
+    setIsPending(false);
+  }, []);
   return (
-    <LoginLayout left={<LandingPageLeft />} right={<LandingPageRight />} />
+    <>
+      {isPending && <div>loading...</div>}
+      {left && right && <LoginLayout left={left} right={right} />}
+    </>
   );
 };
 
