5d0316911f020b0f691720bcaa3142df9b90af8b
Mihaela Catan <mihaelacatan7@gmail.com>
2025-09-12 18:24:48 +0300

    Add CurrentBalance component

---
+++ canteen-management-system-client/src/components/MainPage/CurrentBalance/CurrentBalance.jsx
@@ -0,0 +1,69 @@
+import { Card, Col, Typography, Badge } from "antd";
+import "./CurrentBalance.css";
+
+const { Title, Text } = Typography;
+
+const CurrentBalance = () => {
+  const currentBalance = "MDL 682";
+  return (
+    <Col xs={24} md={16}>
+      <Card className="current-balance-card">
+        <div>
+          <Text className="current-balance-text poppins-medium">
+            Current Balance
+          </Text>
+          <div className="balance-container">
+            <div className="balance-amount">
+              <Title
+                className="poppins-bold"
+                level={2}
+                style={{ color: "white", margin: "0", fontSize: "2rem" }}
+              >
+                {currentBalance}
+              </Title>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M3 0.556641C1.34531 0.556641 0 2.02282 0 3.82617V20.1738C0 21.9772 1.34531 23.4434 3 23.4434H21C22.6547 23.4434 24 21.9772 24 20.1738V8.73047C24 6.92712 22.6547 5.46094 21 5.46094H3.75C3.3375 5.46094 3 5.09312 3 4.64355C3 4.19399 3.3375 3.82617 3.75 3.82617H21C21.8297 3.82617 22.5 3.09564 22.5 2.19141C22.5 1.28718 21.8297 0.556641 21 0.556641H3ZM19.5 12.8174C19.8978 12.8174 20.2794 12.9896 20.5607 13.2962C20.842 13.6028 21 14.0186 21 14.4521C21 14.8857 20.842 15.3015 20.5607 15.6081C20.2794 15.9147 19.8978 16.0869 19.5 16.0869C19.1022 16.0869 18.7206 15.9147 18.4393 15.6081C18.158 15.3015 18 14.8857 18 14.4521C18 14.0186 18.158 13.6028 18.4393 13.2962C18.7206 12.9896 19.1022 12.8174 19.5 12.8174Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="view-transactions-button">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="10"
+            viewBox="0 0 12 10"
+            fill="none"
+          >
+            <path
+              d="M1.7 0.800006C1.7 0.412819 1.38719 0.100006 1 0.100006C0.612816 0.100006 0.300003 0.412819 0.300003 0.800006V8.15001C0.300003 9.11688 1.08313 9.90001 2.05 9.90001H10.8C11.1872 9.90001 11.5 9.58719 11.5 9.20001C11.5 8.81282 11.1872 8.50001 10.8 8.50001H2.05C1.8575 8.50001 1.7 8.34251 1.7 8.15001V0.800006ZM10.5944 2.69438C10.8678 2.42094 10.8678 1.97688 10.5944 1.70344C10.3209 1.43001 9.87688 1.43001 9.60344 1.70344L7.3 4.00907L6.04438 2.75344C5.77094 2.48001 5.32688 2.48001 5.05344 2.75344L2.60344 5.20344C2.33 5.47688 2.33 5.92094 2.60344 6.19438C2.87688 6.46782 3.32094 6.46782 3.59438 6.19438L5.55 4.24094L6.80563 5.49657C7.07907 5.77001 7.52313 5.77001 7.79657 5.49657L10.5966 2.69657L10.5944 2.69438Z"
+              fill="white"
+            />
+          </svg>
+          <Text
+            className="poppins-medium"
+            style={{
+              color: "white",
+              margin: "0",
+              textAlign: "center",
+              marginLeft: "8px",
+            }}
+          >
+            View Transactions
+          </Text>
+        </div>
+      </Card>
+    </Col>
+  );
+};
+
+export default CurrentBalance;
