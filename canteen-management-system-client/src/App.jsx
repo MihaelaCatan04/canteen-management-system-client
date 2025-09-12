07df921463469ede1cc36309c30777a3b4a2e2b0
Mihaela Catan <mihaelacatan7@gmail.com>
2025-09-13 00:39:31 +0300

    Correct border radius for all main page components

--- canteen-management-system-client/src/App.jsx
+++ canteen-management-system-client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import TrayGoApp from "./components/MainPage/draft.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LandingPage />} />
         <Route path="/register" element={<LandingPage />} />
+        <Route path="/draft" element={<TrayGoApp />} />
       </Routes>
     </Router>
   );
